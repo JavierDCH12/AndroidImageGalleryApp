dbd8326d145aa7e80f05ac6d0918d077b8551f60
JavierDCH12 <javierdelgadoch95@gmail.com>
2024-11-09 17:50:52 +0100

    main, 2 frags added

---
+++ app/src/main/java/com/example/imagegallery/FragmentoLauncher.java
@@ -0,0 +1,48 @@
+package com.example.imagegallery;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.imagegallery.databinding.FragmentFragmentoLauncherBinding;
+
+
+public class FragmentoLauncher extends Fragment {
+    FragmentFragmentoLauncherBinding binding;
+    public FragmentoLauncher() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentFragmentoLauncherBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+
+
+}//FRAGMENT END
--- app/src/main/java/com/example/imagegallery/MainActivity.java
+++ app/src/main/java/com/example/imagegallery/MainActivity.java
@@ -1,24 +1,67 @@
 package com.example.imagegallery;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.imagegallery.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+        //BOTTOM MENU
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(binding.navHost.getId());
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomMenu, navController);
+
+        binding.bottomMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() ==){
+
+                    
+                }
+            }
         });
-    }
-}
+
+
+
+
+    }//ONCRATE END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//MAIN END
